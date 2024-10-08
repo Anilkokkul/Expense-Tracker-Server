7c3abaaf30fc5090d970c4638232551b53b42854
Anilkokkul <anilkokkul1997@gmail.com>
2024-10-08 21:56:28 +0530

    updated cors options

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const Routes = require("./routes");
 const { db } = require("./db/db.connect");
 db();
 
-
 const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://creative-malabi-5c3f4d.netlify.app",
+    ],
     credentials: true,
   })
 );
